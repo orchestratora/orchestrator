446b01fcaeea1fadf6226822f3e3deee54e19e67
Alex Malkevich <alex.malkevich@mcmakler.de>
2018-10-17 18:06:33 +0200

    fix(core): types of dynamic components

--- libs/core/src/lib/types.ts
+++ libs/core/src/lib/types.ts
@@ -1,17 +1,24 @@
 import { Type } from '@angular/core';
 
-export interface OrchestratorConfigItem<T extends OrchestratorDynamicComponent<any>, C = any> {
-  component: Type<T> | string;
+export interface OrchestratorConfigItem<C = any> {
+  component: OrchestratorDynamicComponentType<C> | string;
   items?: OrchestratorConfigItem<OrchestratorDynamicComponent<any>>[];
   config?: C;
 }
 
 export interface OrchestratorDynamicComponentInputs<C = any> {
-  items?: OrchestratorConfigItem<OrchestratorDynamicComponent<any>>[];
+  items?: OrchestratorConfigItem<any>[];
   config?: C;
 }
 
-// tslint:disable-next-line:no-empty-interface
-export interface OrchestratorDynamicComponent<T, C = any>
-  extends OrchestratorDynamicComponentInputs<C>,
-    Type<T> {}
+export interface OrchestratorDynamicComponent<C = any>
+  extends OrchestratorDynamicComponentInputs<C> {}
+
+export interface OrchestratorDynamicComponentType<C = any>
+  extends Type<OrchestratorDynamicComponent<C>> {}
+
+export type GetOrchestratorDynamicComponentConfig<T> = T extends Type<
+  OrchestratorDynamicComponent<infer C>
+>
+  ? C
+  : any;
