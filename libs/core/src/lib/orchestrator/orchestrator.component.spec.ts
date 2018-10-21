f2971b78f50b749ae0b405ea85009cf2b680b9f9
gund <malkevich.alex@gmail.com>
2018-10-21 16:07:12 +0200

    feat(core): add componentsCreated output to orchestartor comp

--- libs/core/src/lib/orchestrator/orchestrator.component.spec.ts
+++ libs/core/src/lib/orchestrator/orchestrator.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DynamicModule } from 'ng-dynamic-component';
 
+import { Dynamic1Component, Dynamic2Component } from '../../__test__/dynamic-components';
 import { COMPONENT_MAP } from '../component-map';
 import { RenderItemComponent } from '../render-item/render-item.component';
 import { OrchestratorComponent } from './orchestrator.component';
@@ -12,8 +13,13 @@ describe('OrchestratorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [DynamicModule.withComponents([])],
-      declarations: [OrchestratorComponent, RenderItemComponent],
+      imports: [DynamicModule.withComponents([Dynamic1Component, Dynamic2Component])],
+      declarations: [
+        OrchestratorComponent,
+        RenderItemComponent,
+        Dynamic1Component,
+        Dynamic2Component,
+      ],
       providers: [{ provide: COMPONENT_MAP, useValue: null }],
     }).compileComponents();
   }));
@@ -32,5 +38,20 @@ describe('OrchestratorComponent', () => {
 
     expect(renderItem).toBeTruthy();
     expect(renderItem.componentInstance.item).toBe('my-config');
+  });
+
+  it('should emit `componentsCreated` with all dynamic component refs', () => {
+    component.config = { component: Dynamic1Component, items: [{ component: Dynamic2Component }] };
+
+    const callback = jest.fn();
+    component.componentsCreated.subscribe(callback);
+
+    fixture.detectChanges();
+
+    expect(callback).toHaveReturnedTimes(1);
+    expect(callback).toHaveBeenCalledWith([
+      expect.objectContaining({ instance: expect.any(Dynamic1Component) }),
+      expect.objectContaining({ instance: expect.any(Dynamic2Component) }),
+    ]);
   });
 });
--- libs/core/src/lib/orchestrator/orchestrator.component.ts
+++ libs/core/src/lib/orchestrator/orchestrator.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, Output } from '@angular/core';
+import { ComponentRef } from '@angular/core/src/render3';
+import { combineLatest, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { OrchestratorConfigItem } from '../types';
 
@@ -10,7 +13,23 @@ import { OrchestratorConfigItem } from '../types';
 export class OrchestratorComponent implements OnInit {
   @Input() config: OrchestratorConfigItem<any>;
 
+  private compCreated$ = new Subject<ComponentRef<any>>();
+  private childCompsCreated$ = new Subject<ComponentRef<any>[]>();
+
+  @Output()
+  componentsCreated = combineLatest(this.compCreated$, this.childCompsCreated$).pipe(
+    map(([comp, comps]) => [comp, ...comps]),
+  );
+
   constructor() {}
 
   ngOnInit() {}
+
+  compCreated(compRef: ComponentRef<any>) {
+    this.compCreated$.next(compRef);
+  }
+
+  childCompsCreated(compRefs: ComponentRef<any>[]) {
+    this.childCompsCreated$.next(compRefs);
+  }
 }
