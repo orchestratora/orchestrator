bb4256297487631aa2751629cc45df7cdb6e8ad5
gund <malkevich.alex@gmail.com>
2018-10-21 15:52:29 +0200

    test(core): implement orchestrator tests

--- libs/core/src/lib/orchestrator/orchestrator.component.spec.ts
+++ libs/core/src/lib/orchestrator/orchestrator.component.spec.ts
@@ -1,24 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DynamicModule } from 'ng-dynamic-component';
 
+import { COMPONENT_MAP } from '../component-map';
+import { RenderItemComponent } from '../render-item/render-item.component';
 import { OrchestratorComponent } from './orchestrator.component';
 
-xdescribe('OrchestratorComponent', () => {
+describe('OrchestratorComponent', () => {
   let component: OrchestratorComponent;
   let fixture: ComponentFixture<OrchestratorComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [OrchestratorComponent],
+      imports: [DynamicModule.withComponents([])],
+      declarations: [OrchestratorComponent, RenderItemComponent],
+      providers: [{ provide: COMPONENT_MAP, useValue: null }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrchestratorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render `orc-render-item` with config => item prop', () => {
+    component.config = 'my-config' as any;
+
+    fixture.detectChanges();
+
+    const renderItem = fixture.debugElement.query(By.directive(RenderItemComponent));
+
+    expect(renderItem).toBeTruthy();
+    expect(renderItem.componentInstance.item).toBe('my-config');
   });
 });
