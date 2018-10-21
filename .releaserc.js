4378db7fffbc1782ce15f419e557b77c91a0b0b8
gund <malkevich.alex@gmail.com>
2018-10-21 18:33:21 +0200

    chore(package): fix semantic release with hooks

---
+++ .releaserc.js
@@ -0,0 +1,17 @@
+const hooks = require('semantic-release-monorepo-hooks');
+const output = hooks();
+
+const publish = output.isLastModified
+  ? ['@semantic-release/github', '@semantic-release/npm']
+  : ['@semantic-release/npm'];
+
+module.exports = {
+  branch: 'master',
+  tagFormat: 'v${version}',
+  prepare: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/git'],
+  publish: publish,
+  verifyConditions: ['@semantic-release/npm', '@semantic-release/github'],
+  // verifyRelease: ['@semantic-release/npm', '@semantic-release/github']
+  //   .map(require)
+  //   .map(x => x.verifyConditions),
+};
