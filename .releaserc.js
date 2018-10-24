b80ff3b44912c206c50f01ac60f8eecc291c67a3
gund <malkevich.alex@gmail.com>
2018-10-24 20:54:05 +0200

    chore(release): set cwd option for changelog plugin

--- .releaserc.js
+++ .releaserc.js
@@ -5,11 +5,18 @@ const publish = output.isLastModified
   ? ['@semantic-release/github', '@semantic-release/npm']
   : ['@semantic-release/npm'];
 
+const cwd = process.cwd();
+console.log('USING CWD', cwd);
+
 module.exports = {
   branch: 'master',
   tagFormat: 'v${version}',
-  prepare: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/git'],
+  prepare: [
+    ['@semantic-release/changelog', { cwd }],
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
   publish: publish,
   verifyConditions: ['@semantic-release/npm', '@semantic-release/github'],
-  cwd: process.cwd(),
+  cwd,
 };
