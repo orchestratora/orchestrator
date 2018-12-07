a1893805373761250f47b80191880788edc607eb
gund <malkevich.alex@gmail.com>
2018-12-07 22:30:52 +0100

    feat(core): add @DynamicComponent decorator
    That allows to specify configuration token for dynamic components. Also render-item component will
    now merge config with default config for every dynamic component when @DynamicComponent is used
    
    closes #20

---
+++ libs/core/src/lib/metadata/dynamic-component.ts
@@ -0,0 +1,22 @@
+import { Type } from '@angular/core';
+
+import { OrchestratorDynamicComponentType } from '../types';
+import { createMetadataGetSet } from './util';
+
+export interface DynamicComponentOptions<C> {
+  config: Type<C>;
+}
+
+const dynamicComponentMeta = createMetadataGetSet<DynamicComponentOptions<any>>(
+  'DynamicComponentMeta',
+);
+
+export function DynamicComponent<C>(options: DynamicComponentOptions<C>): ClassDecorator {
+  return target => dynamicComponentMeta.set(options, target);
+}
+
+export function getDynamicComponentMeta<C>(
+  type: OrchestratorDynamicComponentType<C>,
+): DynamicComponentOptions<C> | undefined {
+  return dynamicComponentMeta.get(type);
+}
