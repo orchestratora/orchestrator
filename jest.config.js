6910ddd1f7312a801f214a3b8678760f30aabb3b
gund <malkevich.alex@gmail.com>
2019-01-28 01:23:48 +0100

    test(jest): fix path for external lib namespace

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleNameMapper: {
+    '@orchestrator/gen-io-ts': '<rootDir>/node_modules/@orchestrator/gen-io-ts',
     '@orchestrator/(.*)': '<rootDir>/libs/$1/src/public_api.ts',
   },
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
