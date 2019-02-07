add90133a77829be46ab56d348d702930f9fa38f
gund <malkevich.alex@gmail.com>
2019-02-07 23:52:47 +0100

    test(core): static injector

---
+++ libs/core/src/lib/injectors/static-injector.spec.ts
@@ -0,0 +1,26 @@
+import { getStaticInjector, STATIC_INJECT_FLAGS } from './static-injector';
+import { Injector, InjectFlags } from '@angular/core';
+
+describe('getStaticInjector()', () => {
+  it('should return Injector', () => {
+    const injector = getStaticInjector({} as any);
+
+    expect(injector.get).toEqual(expect.any(Function));
+  });
+
+  it('should set parent injector from argument', () => {
+    const parentInjector = {} as any;
+
+    const injector = getStaticInjector(parentInjector);
+
+    expect((injector as any).parent).toBe(parentInjector);
+  });
+
+  it('should provide `STATIC_INJECT_FLAGS` with value `InjectFlags`', () => {
+    const staticInjector = getStaticInjector({} as any);
+
+    const injectFlags = staticInjector.get(STATIC_INJECT_FLAGS);
+
+    expect(injectFlags).toBe(InjectFlags);
+  });
+});
