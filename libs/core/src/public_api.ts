051faf577832dd3090d1e4cba8e61994276df47a
gund <malkevich.alex@gmail.com>
2019-02-09 00:05:17 +0100

    fix(core): export public API entities

--- libs/core/src/public_api.ts
+++ libs/core/src/public_api.ts
@@ -1,9 +1,23 @@
+// Core
 export * from './lib/core.module';
 export * from './lib/types';
-export * from './lib/render-item/render-item.component';
 export * from './lib/metadata';
+
+// Components
+export * from './lib/orchestrator/orchestrator.component';
+export * from './lib/render-component';
+export * from './lib/render-item/render-item.component';
+
+// Errors
 export * from './lib/error-strategy/error-strategy';
 export * from './lib/error-strategy/throw-error-strategy';
 export * from './lib/error-strategy/suppress-error-strategy';
+
+// Configuration
 export * from './lib/config';
 export * from './lib/config/configuration.service';
+export * from './lib/config/invalid-configuration-error';
+export * from './lib/config/function-error';
+
+// Injectors
+export * from './lib/injectors/injector-registry.service';
