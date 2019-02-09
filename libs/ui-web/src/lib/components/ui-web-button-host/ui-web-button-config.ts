899a420444bd7e6e8396934e9e3252ffcfd991fb
gund <malkevich.alex@gmail.com>
2019-02-09 03:08:10 +0100

    fix(ui-web): remove custom click from button config component
    Because it's now possible to set any handlers on any dynamic component

--- libs/ui-web/src/lib/components/ui-web-button-host/ui-web-button-config.ts
+++ libs/ui-web/src/lib/components/ui-web-button-host/ui-web-button-config.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { OptionFunction } from '@orchestrator/core';
 
 @Injectable()
 export class UiWebButtonConfig {
@@ -7,5 +6,4 @@ export class UiWebButtonConfig {
   type?: string;
   disabled?: boolean;
   tabindex?: number;
-  @OptionFunction() click?: (e: Event) => void = () => {};
 }
