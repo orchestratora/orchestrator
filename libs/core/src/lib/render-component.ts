df2a705cae679623154aab417fbefb27fe8568a5
gund <malkevich.alex@gmail.com>
2019-02-09 03:23:13 +0100

    docs(core): add docstrings to render component class

--- libs/core/src/lib/render-component.ts
+++ libs/core/src/lib/render-component.ts
@@ -1,10 +1,34 @@
 import { InjectorRegistryService } from './injectors/injector-registry.service';
 import { OrchestratorConfigItem } from './types';
 
+/**
+ * Abstract component type that is responsible to render dynamic component
+ */
 export abstract class RenderComponent {
+  /**
+   * Mark for check dynamic component
+   */
   abstract markForCheck(): void;
+
+  /**
+   * Add new item to dynamic component `items` property
+   * causing it to render new component if supported by component
+   */
   abstract addItem(item: OrchestratorConfigItem<any>): void;
+
+  /**
+   * Remove item from dynamic component `item` property
+   * causing it to remove rendered component from view
+   */
   abstract removeItem(item: OrchestratorConfigItem<any>): void;
+
+  /**
+   * Remove all dynamic components from view
+   */
   abstract clearItems(): void;
+
+  /**
+   * Get {@link InjectorRegistryService} to manage injector resolution
+   */
   abstract getInjectorRegistryService(): InjectorRegistryService;
 }
