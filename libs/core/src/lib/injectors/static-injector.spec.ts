88d791045586caee51c2d4bbfa183f95763a98ed
gund <malkevich.alex@gmail.com>
2019-02-18 10:05:44 +0100

    refactor(core): use resolved component type
    And unit test local injector arguments

--- libs/core/src/lib/injectors/static-injector.spec.ts
+++ libs/core/src/lib/injectors/static-injector.spec.ts
@@ -1,9 +1,9 @@
-import { getStaticInjector, STATIC_INJECT_FLAGS } from './static-injector';
+import { createStaticInjector, STATIC_INJECT_FLAGS } from './static-injector';
 import { InjectFlags } from '@angular/core';
 
 describe('getStaticInjector()', () => {
   it('should return Injector', () => {
-    const injector = getStaticInjector({} as any);
+    const injector = createStaticInjector({} as any);
 
     expect(injector.get).toEqual(expect.any(Function));
   });
@@ -11,13 +11,13 @@ describe('getStaticInjector()', () => {
   it('should set parent injector from argument', () => {
     const parentInjector = {} as any;
 
-    const injector = getStaticInjector(parentInjector);
+    const injector = createStaticInjector(parentInjector);
 
     expect((injector as any).parent).toBe(parentInjector);
   });
 
   it('should provide `STATIC_INJECT_FLAGS` with value `InjectFlags`', () => {
-    const staticInjector = getStaticInjector({} as any);
+    const staticInjector = createStaticInjector({} as any);
 
     const injectFlags = staticInjector.get(STATIC_INJECT_FLAGS);
 
