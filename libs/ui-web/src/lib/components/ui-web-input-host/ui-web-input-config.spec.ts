20bad6ac1f85611adaaa1f6b529095154926af60
gund <malkevich.alex@gmail.com>
2019-03-04 02:59:07 +0100

    fix(ui-web): add config validation for input component

--- libs/ui-web/src/lib/components/ui-web-input-host/ui-web-input-config.spec.ts
+++ libs/ui-web/src/lib/components/ui-web-input-host/ui-web-input-config.spec.ts
@@ -1,7 +1,108 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  ConfigurationService,
+  ErrorStrategy,
+  ThrowErrorStrategy,
+} from '@orchestrator/core';
+
 import { UiWebInputConfig } from './ui-web-input-config';
 
 describe('UiWebInputConfig', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ConfigurationService,
+        { provide: ErrorStrategy, useClass: ThrowErrorStrategy },
+      ],
+    });
+  });
+
   it('should exist', () => {
     expect(UiWebInputConfig).toBeTruthy();
   });
+
+  describe('type prop', () => {
+    it('should allow any string value', () => {
+      validConfig({ type: 'type' });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ type: 1 });
+      invalidConfig({ type: true });
+      invalidConfig({ type: {} });
+    });
+  });
+
+  describe('id prop', () => {
+    it('should allow any string value', () => {
+      validConfig({ id: 'id' });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ id: 1 });
+      invalidConfig({ id: true });
+      invalidConfig({ id: {} });
+    });
+  });
+
+  describe('label prop', () => {
+    it('should allow any string value', () => {
+      validConfig({ label: 'label' });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ label: 1 });
+      invalidConfig({ label: true });
+      invalidConfig({ label: {} });
+    });
+  });
+
+  describe('placeholder prop', () => {
+    it('should allow any string value', () => {
+      validConfig({ placeholder: 'placeholder' });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ placeholder: 1 });
+      invalidConfig({ placeholder: true });
+      invalidConfig({ placeholder: {} });
+    });
+  });
+
+  describe('readonly prop', () => {
+    it('should allow any boolean value', () => {
+      validConfig({ readonly: true });
+      validConfig({ readonly: false });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ readonly: 1 });
+      invalidConfig({ readonly: 'value' });
+      invalidConfig({ readonly: {} });
+    });
+  });
+
+  describe('autocomplete prop', () => {
+    it('should allow any string value', () => {
+      validConfig({ autocomplete: 'value' });
+    });
+
+    it('should fail for any other value', () => {
+      invalidConfig({ autocomplete: 1 });
+      invalidConfig({ autocomplete: true });
+      invalidConfig({ autocomplete: {} });
+    });
+  });
 });
+
+function validConfig(config: any) {
+  expect(() => getConfigSvc().validate(UiWebInputConfig, config)).not.toThrow();
+}
+
+function invalidConfig(config: any) {
+  expect(() => getConfigSvc().validate(UiWebInputConfig, config)).toThrow();
+}
+
+function getConfigSvc(): ConfigurationService {
+  return TestBed.get(ConfigurationService);
+}
--- libs/ui-web/src/lib/components/ui-web-input-host/ui-web-input-config.ts
+++ libs/ui-web/src/lib/components/ui-web-input-host/ui-web-input-config.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Option, OptionType } from '@orchestrator/core';
 
 import { GenericFormAttributesConfig, InputType } from '../../types';
 
 @Injectable()
 export class UiWebInputConfig extends GenericFormAttributesConfig {
+  @OptionType(String)
   type?: InputType;
+
+  @Option()
   id?: string;
+
+  @Option()
   label?: string;
+
+  @Option()
   placeholder?: string;
+
+  @Option()
   readonly?: boolean;
+
+  @Option()
   autocomplete?: string;
 }
