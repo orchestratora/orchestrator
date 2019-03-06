7fd1c744f082f018afe67f5c2a2f1b3f58e8578e
gund <malkevich.alex@gmail.com>
2019-03-06 23:06:33 +0100

    fix(ui-web): add config validation for textarea component

--- libs/ui-web/src/lib/components/ui-web-textarea-host/ui-web-textarea-config.spec.ts
+++ libs/ui-web/src/lib/components/ui-web-textarea-host/ui-web-textarea-config.spec.ts
@@ -1,10 +1,20 @@
-import { UiWebTextAreaConfig } from './ui-web-textarea-config';
 import { TestBed } from '@angular/core/testing';
+import {
+  ConfigurationService,
+  ErrorStrategy,
+  ThrowErrorStrategy,
+} from '@orchestrator/core';
+
+import { UiWebTextAreaConfig } from './ui-web-textarea-config';
 
 describe('UiWebTextAreaConfig', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UiWebTextAreaConfig],
+      providers: [
+        UiWebTextAreaConfig,
+        ConfigurationService,
+        { provide: ErrorStrategy, useClass: ThrowErrorStrategy },
+      ],
     });
   });
 
@@ -20,7 +30,233 @@ describe('UiWebTextAreaConfig', () => {
     expect(getConfig().rows).toBe(2);
   });
 
-  function getConfig(): UiWebTextAreaConfig {
-    return TestBed.get(UiWebTextAreaConfig);
-  }
+  describe('placeholder prop', () => {
+    it('should allow any string value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { placeholder: 'val' }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { placeholder: 1.1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { placeholder: true }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { placeholder: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('readonly prop', () => {
+    it('should allow any boolean value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { readonly: true }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { readonly: false }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { readonly: 1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { readonly: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { readonly: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('cols prop', () => {
+    it('should allow positive integer value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: 1 }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: 0 }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: 1.1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: -1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { cols: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('rows prop', () => {
+    it('should allow positive integer value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: 1 }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: 0 }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: 1.1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: -1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { rows: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('maxlength prop', () => {
+    it('should allow positive integer value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: 1 }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: 0 }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: 1.1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: -1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { maxlength: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('minlength prop', () => {
+    it('should allow positive integer value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: 1 }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: 0 }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: 1.1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: -1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { minlength: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('spellcheck prop', () => {
+    it('should allow boolean value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: true }),
+      ).not.toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: false }),
+      ).not.toThrow();
+    });
+
+    it('should allow `default` value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: 'default' }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: 1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: 'val' }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { spellcheck: {} }),
+      ).toThrow();
+    });
+  });
+
+  describe('wrap prop', () => {
+    it('should allow any string value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { wrap: 'val' }),
+      ).not.toThrow();
+    });
+
+    it('should fail for any other value', () => {
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { wrap: 1 }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { wrap: true }),
+      ).toThrow();
+
+      expect(() =>
+        getConfigSvc().validate(UiWebTextAreaConfig, { wrap: {} }),
+      ).toThrow();
+    });
+  });
 });
+
+function getConfig(): UiWebTextAreaConfig {
+  return TestBed.get(UiWebTextAreaConfig);
+}
+
+function getConfigSvc(): ConfigurationService {
+  return TestBed.get(ConfigurationService);
+}
--- libs/ui-web/src/lib/components/ui-web-textarea-host/ui-web-textarea-config.ts
+++ libs/ui-web/src/lib/components/ui-web-textarea-host/ui-web-textarea-config.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
+import {
+  Option,
+  OptionAllowedValues,
+  OptionInteger,
+  OptionRange,
+} from '@orchestrator/core';
 
 import { GenericFormAttributesConfig } from '../../types';
 
 @Injectable()
 export class UiWebTextAreaConfig extends GenericFormAttributesConfig {
+  @Option()
   placeholder?: string;
+
+  @Option()
   readonly?: boolean;
+
+  @OptionRange(0, Infinity, 1)
+  @OptionInteger()
   cols? = 20;
+
+  @OptionRange(0, Infinity, 1)
+  @OptionInteger()
   rows? = 2;
+
+  @OptionRange(0, Infinity, 1)
+  @OptionInteger()
   maxlength?: number;
+
+  @OptionRange(0, Infinity, 1)
+  @OptionInteger()
   minlength?: number;
+
+  @OptionAllowedValues(Boolean, 'default')
   spellcheck?: boolean | 'default';
+
+  @Option()
   wrap?: string;
 }
