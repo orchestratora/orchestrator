b31309a11aa6d232a36f489b744c40e40b50dde6
gund <malkevich.alex@gmail.com>
2019-03-19 01:22:45 +0100

    feat(composer): add ant design to example app

--- apps/composer-example/src/main.ts
+++ apps/composer-example/src/main.ts
@@ -1,5 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { enableProdMode, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { en_US, NZ_I18N } from 'ng-zorro-antd';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,6 +11,13 @@ if (environment.production) {
   enableProdMode();
 }
 
+registerLocaleData(en);
+
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    providers: [
+      { provide: LOCALE_ID, useValue: 'en-US' },
+      { provide: NZ_I18N, useValue: en_US },
+    ],
+  })
   .catch(err => console.error(err));
