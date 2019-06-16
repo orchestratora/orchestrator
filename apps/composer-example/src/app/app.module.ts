d40d8a0c2c6ff0bf2e819769606c4a231af9ff8b
gund <malkevich.alex@gmail.com>
2019-06-16 22:40:39 +0200

    docs(composer): add browser animations module to app

--- apps/composer-example/src/app/app.module.ts
+++ apps/composer-example/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       {
