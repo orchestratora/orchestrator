628a428d5e66373f48f544a47b919834835596ba
gund <malkevich.alex@gmail.com>
2019-06-18 03:30:51 +0200

    docs(composer): add http client module for ant icons module

--- apps/composer-example/src/app/app.module.ts
+++ apps/composer-example/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       {
