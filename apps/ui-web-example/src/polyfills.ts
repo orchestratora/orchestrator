f8da8c5fb3522eddf0f8ab20dd6b861b50da0e14
Alex Malkevich <malkevich.alex@gmail.com>
2020-03-09 05:34:43 +0100

    chore(ui-web-example): add globalThis polyfill

--- apps/ui-web-example/src/polyfills.ts
+++ apps/ui-web-example/src/polyfills.ts
@@ -33,6 +33,7 @@
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+import 'core-js/proposals/global-this';
 
 /**
  * If the application will be indexed by Google Search, the following is required.
