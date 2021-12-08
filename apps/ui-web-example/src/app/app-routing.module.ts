8966a9d52e1ed072613a86547c1bc27994975c23
Alex Malkevich <malkevich.alex@gmail.com>
2021-12-08 11:32:44 +0100

    test(app): fix app e2e tests

--- apps/ui-web-example/src/app/app-routing.module.ts
+++ apps/ui-web-example/src/app/app-routing.module.ts
@@ -20,7 +20,9 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
