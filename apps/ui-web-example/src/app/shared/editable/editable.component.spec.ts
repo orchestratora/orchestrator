8733f835f74946cf765fa36be017d120e1c0b216
Alex Malkevich <malkevich.alex@gmail.com>
2021-12-13 20:43:39 +0100

    test(ui-web-example): fix linting errors

--- apps/ui-web-example/src/app/shared/editable/editable.component.spec.ts
+++ apps/ui-web-example/src/app/shared/editable/editable.component.spec.ts
@@ -1,17 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { EditableComponent } from './editable.component';
 
 xdescribe('EditableComponent', () => {
   let component: EditableComponent;
   let fixture: ComponentFixture<EditableComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditableComponent],
-      teardown: { destroyAfterEach: false },
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [EditableComponent],
+        teardown: { destroyAfterEach: false },
+      }).compileComponents();
+    }),
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditableComponent);
