30bfd36e09f2619c885beae14f708b59053c2d9f
Alex Malkevich <malkevich.alex@gmail.com>
2022-01-15 00:23:52 +0100

    test(e2e): update composer e2e app files

--- apps/composer-example-e2e/src/integration/app.spec.ts
+++ apps/composer-example-e2e/src/integration/app.spec.ts
@@ -1,9 +1,13 @@
 import { getGreeting } from '../support/app.po';
 
-describe('Hello Nx', () => {
+describe('test', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to composer-example!');
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome to test!');
   });
 });
